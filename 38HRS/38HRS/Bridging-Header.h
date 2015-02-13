4be9d12370905dd4ecba628566f0bcc055dac805
Nicolas St-Aubin <nicolas@Nicolass-MacBook-Pro-2.local>
2015-02-13 14:10:01 -0500

    initial contentful setup

---
+++ 38HRS/38HRS/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  ContentfulTest
+//
+//  Created by Nicolas St-Aubin on 2015-02-10.
+//  Copyright (c) 2015 Nicolas St-Aubin. All rights reserved.
+//
+
+#ifndef ContentfulTest_Bridging_Header_h
+#define ContentfulTest_Bridging_Header_h
+
+#import <ContentfulDeliveryAPI/ContentfulDeliveryAPI.h>
+
+#endif
---
+++ 38HRS/38HRS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  38HRS
+//
+//  Created by Nicolas St-Aubin on 2015-02-13.
+//  Copyright (c) 2015 Nicolas St-Aubin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
