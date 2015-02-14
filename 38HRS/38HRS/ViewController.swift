f7ec63e61cd57b7776c1f1215ed46cadfb12bae4
Nicolas St-Aubin <nicolas@Nicolass-MacBook-Pro-2.local>
2015-02-14 14:20:36 -0500

    Initial sync from Contentful and saved for offline use

--- 38HRS/38HRS/ViewController.swift
+++ 38HRS/38HRS/ViewController.swift
@@ -13,17 +13,40 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var defaults = NSUserDefaults.standardUserDefaults()
+        
         var client = CDAClient(spaceKey:"kmyecir7ff4o", accessToken:"9c37d701db210840f3b6f52c7f1538b937542f0af1e96a8e10615d9ec69cc0e7")
         
-        var contentSpace: CDASyncedSpace!
+        
+        if defaults.objectForKey("entries") != nil {
+            
+            println("From defaults : ")
+            
+            var entriData = defaults.objectForKey("entries") as NSData
+            var entries = NSKeyedUnarchiver.unarchiveObjectWithData(entriData) as NSArray
+            println(entries)
+            
+        }else{
         
         client.initialSynchronizationWithSuccess({ (response: CDAResponse!, space : CDASyncedSpace!) -> Void in
-            println(space.syncToken)
-            println(space.entries)
-            contentSpace = space
+            
+            println("From web : ")
+            println(space.entries);
+            
+            var entriData = NSKeyedArchiver.archivedDataWithRootObject(space.entries)
+            
+            defaults.setObject(entriData, forKey: "entries")
+            defaults.synchronize()
+            
+            //var alex = ami(name: "coucou",age: 22)
+            
+            //var manager = CDAPersistenceManager()
+            
             }, failure: { (response: CDAResponse!, error: NSError!) -> Void in
                 println(error)
         })
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
---
+++ 38HRS/38HRS/ami.swift
@@ -0,0 +1,20 @@
+//
+//  ami.swift
+//  38HRS
+//
+//  Created by Nicolas St-Aubin on 2015-02-14.
+//  Copyright (c) 2015 Nicolas St-Aubin. All rights reserved.
+//
+
+import Foundation
+
+class ami {
+    
+    internal var name : String!
+    internal var age : Int!
+    
+    init(name: String, age: Int){
+        self.name = name
+        self.age = age
+    }
+}
